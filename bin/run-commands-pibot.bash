d5f33e7cf551f4a1af6f984cb822aac16610aa6c
Guillaume Lozenguez <guillaume.lozenguez@imt-lille-douai.fr>
2023-06-28 11:28:48 +0200

    pibot domain id

--- bin/run-commands-pibot.bash
+++ bin/run-commands-pibot.bash
@@ -1,7 +1,13 @@
+# Setup ROS command environment:
 source /opt/ros/iron/setup.bash
 if [ -d "~/mb6-space/install" ]; then
   source ~/mb6-space/install/setup.bash
 fi
 
-export PIBOT_NUMBER=`~/mb6-space/bin/pibot-number`
-echo $PIBOT_NUMBER
+# Pibot configuration:
+
+export ROS_DOMAIN_ID=`~/mb6-space/bin/pibot-number`
+
+echo "
+  ---  Pibot ready on ROS domain: $ROS_DOMAIN_ID  ---
+"
--- bin/run-commands.bash
+++ bin/run-commands.bash
@@ -1,4 +1,7 @@
+# Setup ROS command environment:
 source /opt/ros/iron/setup.bash
 if [ -d "~/mb6-space/install" ]; then
   source ~/mb6-space/install/setup.bash
 fi
+
+# User configuration:
