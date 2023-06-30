4899af2bd107792feba9e45836d5382fd2b019f1
Guillaume Lozenguez <guillaume.lozenguez@imt-lille-douai.fr>
2023-06-30 17:53:21 +0200

    communicative run-commands

--- bin/run-commands-pibot.bash
+++ bin/run-commands-pibot.bash
@@ -1,10 +1,11 @@
 # Setup ROS command environment:
 
 source /opt/ros/iron/setup.bash
-if [ -d "~/mb6-space/install" ]; then
-  source ~/mb6-space/install/setup.bash
+export ROS_WORKSPACE_NAME="iron"
+if [ -e ~/Projects/mb6-space/install/setup.bash ]; then
+  source ~/Projects/mb6-space/install/setup.bash
+  export ROS_WORKSPACE_NAME="mb6-space"
 fi
-
 
 # Pibot configurations:
 
@@ -14,5 +15,5 @@ export ROS_DOMAIN_ID=`~/mb6-space/bin/pibot-number`
 # go message:
 
 echo "
-  ---  `hostname` ready on ROS domain: $ROS_DOMAIN_ID  ---
+  ---  `hostname`:$ROS_WORKSPACE_NAME ready on ROS domain: $ROS_DOMAIN_ID  ---
 "
--- bin/run-commands.bash
+++ bin/run-commands.bash
@@ -1,8 +1,18 @@
 # Setup ROS command environment:
 
 source /opt/ros/iron/setup.bash
-if [ -d "~/mb6-space/install" ]; then
-  source ~/mb6-space/install/setup.bash
+export ROS_WORKSPACE_NAME="iron"
+if [ -e ~/Projects/mb6-space/install/setup.bash ]; then
+  source ~/Projects/mb6-space/install/setup.bash
+  export ROS_WORKSPACE_NAME="mb6-space"
 fi
 
 # User configurations:
+
+
+
+# go message:
+
+echo "
+  ---  `hostname`:$ROS_WORKSPACE_NAME ready on ROS domain: $ROS_DOMAIN_ID  ---
+"
