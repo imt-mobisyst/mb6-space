6dd39aa19f690f8bf554fdcab0dbecb57fdd375b
Guillaume Lozenguez <guillaume.lozenguez@imt-lille-douai.fr>
2023-12-07 16:24:28 +0100

    local config file

--- bin/pibot-run-commands.bash
+++ bin/pibot-run-commands.bash
@@ -21,10 +21,7 @@ alias mb6-build-pkg='colcon build --parallel-workers 1 --executor sequential --e
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:local($ROS_LOCALHOST_ONLY),domain($ROS_DOMAIN_ID):\[\033[01;34m\]\w\[\033[00m\]\n\$ '
 
 # Pibot configurations:
-
-export ROS_LOCALHOST_ONLY=0
 export ROS_DOMAIN_ID=`~/mb6-space/bin/pibot-number`
-
 
 # go message:
 
