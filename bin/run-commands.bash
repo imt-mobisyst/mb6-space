52af61c22e4b68665659e09c7b3cb0ad48bfb52f
Guillaume Lozenguez <guillaume.lozenguez@imt-lille-douai.fr>
2023-12-07 16:24:28 +0100

    local config file

--- bin/run-commands.bash
+++ bin/run-commands.bash
@@ -1,9 +1,13 @@
-# Setup ROS command environment:
+# Go at the root directory of the workspace
+cd `dirname $0`/..
 
-source /opt/ros/iron/setup.bash
-export ROS_WORKSPACE="iron"
-if [ -e ~/mb6-space/install/setup.bash ]; then
-  source ~/mb6-space/install/setup.bash
+# Setup ROS command environment:
+if [ -e /opt/ros/iron/setup.bash ]; then
+  source /opt/ros/iron/setup.bash
+  export ROS_WORKSPACE="iron"
+fi
+if [ -e ./install/setup.bash ]; then
+  source ./install/setup.bash
   export ROS_WORKSPACE="mb6-space"
 fi
 
@@ -17,16 +21,21 @@ alias diff='git difftool -t meld'
 alias mb6-build-pkg='colcon build --event-handlers console_direct+ --cmake-args -DCMAKE_VERBOSE_MAKEFILE=ON --packages-select'
 
 # Tunned prompt:
-
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\].\n\$ '
 
 # User configurations:
+if [ -e ./config-ros.bash ]; then
+  source ./config-ros.bash
+else
+  echo "# ROS configuration:
+export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
+export ROS_DOMAIN_ID=00
+" > ./config-ros.bash
+  source ./config-ros.bash
+fi
 
-export ROS_LOCALHOST_ONLY=1
-#export ROS_DOMAIN_ID=3
-
-# go message:
-if [ "$ROS_LOCALHOST_ONLY" = 1 ]; then
+# Go message:
+if [ "$ROS_LOCALHOST_ONLY" = "LOCALHOST" ]; then
   echo "
     ---  `hostname`:$ROS_WORKSPACE ready on LocalHost ONLY  ---
   "
