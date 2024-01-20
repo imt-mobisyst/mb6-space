7796280fa2514d7ec4a6ad577d75ee9e41ccda8e
guillaumeLozengue <guillaume.lozengue@imt-nord-europe.fr>
2024-01-20 13:47:26 +0100

    Patches mechanism

--- bin/station-start.sh
+++ bin/station-start.sh
@@ -7,23 +7,22 @@ git pull
 # Update mb6's packages...
 if [ -e ./pkg-basic ]; then
     cd pkg-basic
+    echo "#    P K G  -  B A S I C"
     git pull
     cd ..
 fi
 if [ -e ./pkg-tbot ]; then
     cd pkg-tbot
+    echo "#    P K G  -  T B O T"
     git pull
     cd ..
 fi
 if [ -e ./pkg-tsim ]; then
     cd pkg-tsim
+    echo "#    P K G  -  T S I M"
     git pull
     cd ..
 fi
 
-# Apply patches:
-if [ ! -e ./patches ]; then
-    mkdir patches
-fi
-
+echo "#    P A T C H E S"
 bash ./bin/station-patches.sh
