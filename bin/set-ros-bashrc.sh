d052871eb84b0ed2fa899f49623e9faca59b05e3
Luc Fabresse <luc.fabresse@imt-nord-europe.fr>
2024-01-23 11:34:57 +0100

    update ros user bashrc

---
+++ bin/set-ros-bashrc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo -u ros cat << END >> /home/ros/.bashrc
+
+ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
+ROS_DOMAIN_ID=00
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:($ROS_AUTOMATIC_DISCOVERY_RANGE::$ROS_DOMAIN_ID)\[\033[01;34m\]\w\[\033[00m\].\n\$ '
+END
