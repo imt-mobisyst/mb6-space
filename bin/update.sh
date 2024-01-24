1cecda6f90460f55a31e6385134f92e42af7b836
guillaumeLozengue <guillaume.lozengue@imt-nord-europe.fr>
2024-01-24 17:03:27 +0100

    wip

--- bin/update.sh
+++ bin/update.sh
@@ -9,20 +9,21 @@ if [ -e ./pkg-basic ]; then
     cd pkg-basic
     echo "#    P K G  -  B A S I C"
     git pull
+    ./bin/build
     cd ..
 fi
-if [ -e ./pkg-tbot ]; then
-    cd pkg-tbot
-    echo "#    P K G  -  T B O T"
-    git pull
-    cd ..
-fi
-if [ -e ./pkg-tsim ]; then
-    cd pkg-tsim
-    echo "#    P K G  -  T S I M"
-    git pull
-    cd ..
-fi
+# if [ -e ./pkg-tbot ]; then
+#    cd pkg-tbot
+#    echo "#    P K G  -  T B O T"
+#    git pull
+#    cd ..
+# fi
+# if [ -e ./pkg-tsim ]; then
+#    cd pkg-tsim
+#    echo "#    P K G  -  T S I M"
+#    git pull
+#    cd ..
+# fi
 
 echo "#    P A T C H E S"
 bash ./bin/station-patch.sh
