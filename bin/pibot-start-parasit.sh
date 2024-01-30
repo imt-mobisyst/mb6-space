0413b06335f41405d8c97e5b2c77ea2633739f74
guillaumeLozengue <guillaume.lozengue@imt-nord-europe.fr>
2024-01-30 09:53:29 +0100

    wip

--- bin/pibot-start-parasit.sh
+++ bin/pibot-start-parasit.sh
@@ -3,14 +3,13 @@
 # Update mb6-space.
 cd `dirname $0`/..
 git pull
-source ./bin/pibot-run-commands.bash
-
-echo "#    P A T C H E S"
-bash ./bin/pibot-patch.sh
 
 echo "#    I N I T I A L I Z E   R O S"
 source ./bin/pibot-run-commands.bash
 sudo ntpdate fr.pool.ntp.org
+
+echo "#    P A T C H E S"
+bash ./bin/pibot-patch.sh
 
 echo "#    R U N   P A R A S I T"
 ros2 run basic_node parasit
