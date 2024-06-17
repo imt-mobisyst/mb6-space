ab6de1d3d828e8461b2719e07d8e36df3403bb85
guillaumeLozengue <guillaume.lozengue@imt-nord-europe.fr>
2024-06-17 10:01:51 +0200

    wip

--- bin/start-parasit.sh
+++ bin/start-parasit.sh
@@ -2,31 +2,25 @@
 
 # Update mb6-space.
 cd `dirname $0`/..
+
+echo "#    P U L L"
+echo "#    mb6-space"
 git pull
 
 # Update mb6's packages...
-if [ -e ./pkg-basic ]; then
-    cd pkg-basic
-    echo "#    P K G  -  B A S I C"
-    git pull
-    cd ..
+for pkg in pkg-basic pkg-tbot pkg-tsim 
+do 
+if [ -e ./$pkg ]; then
+    echo "#    $pkg"
+    git -C ./$pkg pull
 fi
-if [ -e ./pkg-tbot ]; then
-   cd pkg-tbot
-   echo "#    P K G  -  T B O T"
-   git pull
-   cd ..
-fi
-if [ -e ./pkg-tsim ]; then
-   cd pkg-tsim
-   echo "#    P K G  -  T S I M"
-   git pull
-   cd ..
-fi
-colcon build
+done
 
 echo "#    P A T C H E S"
 bash ./bin/station-patch.sh
+
+echo "#    B U I L D"
+colcon build
 
 echo "#    I N I T I A L I Z E   R O S"
 source ./bin/station-run-commands.bash
