f25a50b15bc2aea9c21d4a9dabe2af3ac441b962
guillaumeLozengue <guillaume.lozengue@imt-nord-europe.fr>
2024-06-18 14:34:01 +0200

    wip

--- bin/start-parasit.sh
+++ bin/start-parasit.sh
@@ -7,33 +7,12 @@ echo "#    P U L L"
 echo "#    mb6-space"
 git pull
 
-<<<<<<< HEAD
 echo "#    I N I T I A L I Z E   R O S"
 source ./bin/pibot-run-commands.bash
 sudo ntpdate fr.pool.ntp.org
 
 echo "#    P A T C H E S"
 bash ./bin/pibot-patch.sh
-=======
-# Update mb6's packages...
-for pkg in pkg-basic pkg-tbot pkg-tsim 
-do 
-if [ -e ./$pkg ]; then
-    echo "#    $pkg"
-    git -C ./$pkg pull
-fi
-done
-
-echo "#    P A T C H E S"
-bash ./bin/station-patch.sh
-
-echo "#    B U I L D"
-colcon build
-
-echo "#    I N I T I A L I Z E   R O S"
-source ./bin/run-commands.bash
-#sudo ntpdate fr.pool.ntp.org
->>>>>>> master
 
 echo "#    R U N   P A R A S I T"
 export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET
