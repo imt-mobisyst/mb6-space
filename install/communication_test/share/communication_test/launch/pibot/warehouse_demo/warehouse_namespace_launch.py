54c932366049bb15c39f90ddcb25db565fe4a672
Lucas NAURY <lucas.naury@sfr.fr>
2024-06-21 09:45:47 +0200

    Update pkg-multibot build

--- install/communication_test/share/communication_test/launch/pibot/warehouse_demo/warehouse_namespace_launch.py
+++ install/communication_test/share/communication_test/launch/pibot/warehouse_demo/warehouse_namespace_launch.py
@@ -4,7 +4,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node, PushRosNamespace
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, OpaqueFunction, SetLaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
@@ -15,21 +15,41 @@ def generate_launch_description():
         'nav_log_level', default_value='info', description='log level'
     )
 
+    robot_id_launch_arg = DeclareLaunchArgument(
+        'robot_id', default_value=None
+    )
+
+
     # Robot ID
-    # robot_id = int(socket.gethostname()[-2:])
-    robot_id = 22
-    namespace = f"robot_{robot_id}"
+    def getRobotId(context):
+        id = LaunchConfiguration('robot_id').perform(context)
+
+        if id is None:
+            id = int(socket.gethostname()[-2:]) # Default value = Last number of the kobuki RPI hostname
+
+
+        namespace = f"robot_{id}"
+        
+        print(f'Using robot {id} on namespace "{namespace}"')
+
+        return [
+            SetLaunchConfiguration('id', id),
+            SetLaunchConfiguration('namespace', namespace)
+        ]
+    
+    robot_id = OpaqueFunction(function=getRobotId)
+
 
     # Start a controller node
     controller_node = GroupAction([
-        PushRosNamespace(namespace),
+        PushRosNamespace(LaunchConfiguration('namespace')),
         Node(
             package='communication_test',
-            executable='stage_controller.py',
-            name='stage_controller',
+            executable='kobuki_warehouse_controller.py',
+            name='warehouse_controller',
             parameters=[
                 {
-                    'robot_id': robot_id,
+                    'robot_id': LaunchConfiguration('id'),
                 }
             ]
         )
@@ -37,14 +57,14 @@ def generate_launch_description():
     
 
     localization = GroupAction([
-        PushRosNamespace(namespace),
+        PushRosNamespace(LaunchConfiguration('namespace')),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                         os.path.join(
                             get_package_share_directory('nav2_bringup'),
                             'launch/localization_launch.py')),
             launch_arguments={
-                "namespace": namespace,
+                "namespace": LaunchConfiguration('namespace'),
                 'map': os.path.join(get_package_share_directory('communication_test'), 'map', 'map.yaml'),
                 'params_file': os.path.join(get_package_share_directory('communication_test'), 'config', 'nav2', 'nav2_params_kobuki.yaml'),
                 'autostart': 'True',
@@ -62,7 +82,7 @@ def generate_launch_description():
                     get_package_share_directory('communication_test'),
                     'launch/nav/navigation_launch.py')),
             launch_arguments={
-                "namespace": namespace,
+                "namespace": LaunchConfiguration('namespace'),
                 "params_file": os.path.join(get_package_share_directory('communication_test'), 'config', 'nav2', 'nav2_params_kobuki.yaml'),
                 "log_level": LaunchConfiguration('nav_log_level'),
                 # "use_composition": "True"
@@ -74,6 +94,9 @@ def generate_launch_description():
 
     return LaunchDescription([
         log_level_launch_arg,
+        robot_id_launch_arg,
+
+        robot_id,
 
         controller_node,
 
