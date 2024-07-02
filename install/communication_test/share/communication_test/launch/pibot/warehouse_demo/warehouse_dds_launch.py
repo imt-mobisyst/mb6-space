2079ed3566cce22cbec98c66fc8159d1d66f8e8a
Lucas NAURY <lucas.naury@sfr.fr>
2024-07-02 10:05:46 +0200

    Fix IP on the robot

--- install/communication_test/share/communication_test/launch/pibot/warehouse_demo/warehouse_dds_launch.py
+++ install/communication_test/share/communication_test/launch/pibot/warehouse_demo/warehouse_dds_launch.py
@@ -1,4 +1,5 @@
 import os, socket
+from subprocess import check_output
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction
@@ -14,14 +15,13 @@ def generate_launch_description():
         'nav_log_level', default_value='info', description='log level'
     )
 
-    this_ip = socket.gethostbyname(socket.gethostname())
+    this_ip = check_output(['hostname', '-I'])
 
     # CLI arguments
     subnet_dds_server_launch_arg = DeclareLaunchArgument("subnet_dds_server")
     robot_ip_launch_arg = DeclareLaunchArgument('robot_ip', default_value=this_ip)
     robot_port_launch_arg = DeclareLaunchArgument('robot_port', default_value='11811')
-
-    # Robot ID
+    
 
     # Robot ID
     def setRobotId(context):
@@ -141,8 +141,6 @@ def generate_launch_description():
 
         common_servers_arg,
         local_servers_arg,
-
-        DDSserver,
 
 
         GroupAction([
