956e0ec1c4edaae45bc56ffb008cd9e1ee9f87fd
Lucas NAURY <lucas.naury@sfr.fr>
2024-07-17 15:42:20 +0200

    Update start tbot script

--- bin/start-tbot.sh
+++ bin/start-tbot.sh
@@ -10,7 +10,8 @@ source ./bin/pibot-run-commands.bash
 echo "#    R U N   T B O T"
 
 # Multibot launch
-ROS_DOMAIN_ID=99 ros2 launch communication_test multibot.launch.py type:="namespace"
+# ROS_DOMAIN_ID=99 ros2 launch communication_test multibot.launch.py type:="namespace"
 #ros2 launch communication_test multibot.launch.py type:="domain_id"
 #ROS_DOMAIN_ID=99 ros2 launch communication_test multibot.launch.py type:="discovery"
 #ROS_DOMAIN_ID=99 ros2 launch communication_test multibot.launch.py type:="partitions"
+ros2 launch communication_test multibot.launch.py type:="zenoh"
