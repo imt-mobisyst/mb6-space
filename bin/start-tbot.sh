deef80ff152b3ea056ac40f6a8634494ba25c6c1
guillaumeLozengue <guillaume.lozengue@imt-nord-europe.fr>
2024-12-16 16:39:24 +0100

    with joy control

--- bin/start-tbot.sh
+++ bin/start-tbot.sh
@@ -7,4 +7,4 @@ echo "#    I N I T I A L I Z E   R O S"
 source ./bin/pibot-run-commands.bash
 
 echo "#    R U N   T B O T"
-ros2 launch tbot_node minimal_launch.yaml
+ros2 launch tbot_node pibot_launch.yaml
